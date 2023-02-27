abf95fa9f9311d6241b22383e51a60babc01a01f
Vasil Pashov <vasil.pashov1@gmail.com>
2023-02-27 03:45:36 +0200

    Fix the custom implementation for grid sync (for CUDA_ARCH < 600)

--- gpu_cpu_shared/matrix_math_common.cuh
+++ gpu_cpu_shared/matrix_math_common.cuh
@@ -1,18 +1,27 @@
 #ifndef MATRIX_MATH_COMMON_H
 #define MATRIX_MATH_COMMON_H
+#include<type_traits>
+
+/// When the GPU does not support cooperative groups we want all intermediate
+/// computations to be volatile. Otherwise when we call sync the threads will
+/// be synced, but some of the memory will be cached in each block and the
+/// cache will be different than the real value.
+/// TODO: Remove the custom sync and the need for volatile pointers
+template<bool useVolatile = false>
 struct CGParams {
-    int* rowStart;
-    int* columnIndex;
-    float* values;
-    float* x;
-    float* p;
-    float* ap;
-    float* r;
-    float* residualNormSquared;
-    float* newResidualNormSquared;
-    float* pAp;
-    unsigned int* barrier;
-    unsigned int* generation;
+    using T = std::conditional_t<useVolatile, volatile float*, float*>;
+    const int* rowStart;
+    const int* columnIndex;
+    const float* values;
+    T x;
+    T p;
+    T ap;
+    T r;
+    T residualNormSquared;
+    T newResidualNormSquared;
+    T pAp;
+    unsigned int* barrier; // TODO remove not needed
+    unsigned int* generation; // TODO remove not needed
     int rows;
     int maxIterations;
     float epsSq;
